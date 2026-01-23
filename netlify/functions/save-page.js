0c002083245c5f5546eb0a2d69a5bef373cca329
Your Name <your-email@example.com>
2026-01-23 19:19:08 +0100

    Remove garbage code causing syntax error

--- netlify/functions/save-page.js
+++ netlify/functions/save-page.js
@@ -82,7 +82,6 @@ exports.handler = async (event, context) => {
     };
   }
 };
-        method: method,
         headers: {
           'Authorization': `token ${githubToken}`,
           'Accept': 'application/vnd.github.v3+json',
